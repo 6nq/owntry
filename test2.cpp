774ed444da461c3479a1dc66f2ef26012e003c06
kuduren <1471686500@qq.com>
2021-05-13 23:36:50 +0800

    s

--- test2.cpp
+++ test2.cpp
@@ -10,12 +10,7 @@ auto own_search( typename std::vector<T,std::allocator<T>> ::iterator
                 typename std::vector<T,std::allocator<T>> ::iterator
                     const& right,
                 T const& target,
-                fun_obj const& compare_ 
-                    /* = [](auto const& x, auto const &y)->int{ */
-                    /*     return x-y; */
-                    /* } */
-                ){
-
+                fun_obj const& compare_ ){
     if(left > right){
         printf("out\n");
         if(compare_((*left),(*right)) > 0){
@@ -27,7 +22,6 @@ auto own_search( typename std::vector<T,std::allocator<T>> ::iterator
     auto middle = left + ((right - left)/2);
 
     int flag = compare_((*middle), target);
-
     if(flag == 0){
         printf("find\n");
         return make_pair(middle,middle);
@@ -38,39 +32,53 @@ auto own_search( typename std::vector<T,std::allocator<T>> ::iterator
     }
 }
 
+template<typename T>
+auto own_search( typename std::vector<T,std::allocator<T>> ::iterator
+                    const& left,
+                typename std::vector<T,std::allocator<T>> ::iterator
+                    const& right,
+                T const& target
+        ){
+    return own_search(left,right,target,[](T const& x, T const &y)->int{
+                                                return x-y;
+                                               });
+}
+
 struct Point{
     int x;
     int y;
     Point(int const& a,int const&b):x(a),y(b){ }
 };
 
+
 int main(void)
 {
-    vector<Point> s = {{2,3},{3,1},{3,3},{4 ,5}, {6,7}};
-    Point tag(3,1);
+    /* vector<Point> s = {{2,3},{3,1},{3,3},{4 ,5}, {6,7}}; */
+    /* Point tag(3,1); */
+    /* printf("search begin\n"); */
+    /* auto&& x = own_search(s.begin(), s.end(), tag, [](auto const& a,auto const& b)->int{ */
+    /*                                                     printf("cmp\n"); */
+    /*                                                     int i = (a.x - b.x); */
+    /*                                                     if(i == 0){ */
+    /*                                                         return a.y - b.y; */
+    /*                                                     } */
+    /*                                                     return i; */
+    /*                                                 }); */
+
+    /* printf("index of left_near %ld  index of right_near %ld\n",(x.first - s.begin()),(x.second - s.begin())); */
+
+    vector<int> v = {1,3,5 ,8,13,43};   
+
     printf("search begin\n");
-    auto&& x = own_search(s.begin(), s.end(), tag, [](auto const& a,auto const& b)->int{
-                                                        printf("cmp\n");
-                                                        int i = (a.x - b.x);
-                                                        if(i == 0){
-                                                            return a.y - b.y;
-                                                        }
-                                                        return i;
-                                                    });
-
-    printf("%ld %ld\n",(x.first - s.begin()),(x.second - s.begin()));
-
-/*     vector<int> v = {1,3,5 ,8,13,43}; */   
-
-/*     printf("search begin\n"); */
-/*     auto&& y = own_search(v.begin(), v.end(), 5, [](auto const& x,auto const& y)->int { */
-/*                                                         printf("cmp\n"); */
-/*                                                         return (x-y); */ 
-/*                                                     }); */
-/*     /1* auto&& x = own_search(v.begin(), v.end()-1, 5); *1/ */
-/*     /1* error *1/ */
-/*     printf("%d %d\n",*y.first,*y.second); */
-/*     printf("%ld %ld\n",(y.first - v.begin()),(y.second - v.begin())); */
+    auto&& y = own_search(v.begin(), v.end(), 5);
+    /* auto&& y = own_search(v.begin(), v.end(), 5, [](auto const& x,auto const& y)->int { */
+    /*                                                     printf("cmp\n"); */
+    /*                                                     return (x-y); */ 
+    /*                                                 }); */
+    /* auto&& x = own_search(v.begin(), v.end()-1, 5); */
+    /* error */
+    printf("%d %d\n",*y.first,*y.second);
+    printf("%ld %ld\n",(y.first - v.begin()),(y.second - v.begin()));
     return 0;
 }
 
